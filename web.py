421d8e4a553ebd853bbef5a93ab0ca145ffa0a27
JLEXY <alexander.alabi85@gmail.com>
2024-06-13 01:31:57 +0100

    initial commit

--- web.py
+++ web.py
@@ -12,7 +12,9 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This is my todo app")
-st.write("This app is to increase your productivity")
+st.write("This app is to increase your productivity.")
+st.write("Type in your todos in the input to create your todo list.")
+st.write("Click on any todo to mark it as done.")
 
 
 for index, todo in enumerate(todos):
